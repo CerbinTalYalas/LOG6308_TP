fea02acfe263c5130944b7f3c9949e68f6eb6df2
moi <h.canneddu@gmail.com>
2021-01-28 18:25:44 +0100

    q1

---
+++ TP1/cross_valid.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import math
+import warnings
+from scipy.sparse import csr_matrix
+
+
+# Region[Red] K-fold
+
+def k_fold(nb_fold, data):
+    shuffle_data = data.sample(frac=1, random_state=10)
+    folds = []
+    item_per_fold = math.floor(len(shuffle_data) / nb_fold)
+    start, end = 0, item_per_fold
+    for i in range(nb_fold):
+        folds.append(shuffle_data.iloc[start:end, :])
+        start = end
+        end = len(data) if (i + 1 == nb_fold - 1) else end + item_per_fold
+    return folds
+
+
+# EndRegion
+
+# Region[Green] Cross-validation
+
+def train(data_train, users, items):
+    model = csr_matrix((data_train['rating'], (data_train['user.id'], data_train['item.id']))).toarray()
+    return model
+
+
+def predict(uid, iid, model):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        avg_u = np.mean(model[uid][model[uid] > 0])
+        avg_i = np.mean(model[:, iid][model[:, iid] > 0])
+
+    if np.isnan(avg_u):
+        avg_u = 0
+    if np.isnan(avg_i):
+        avg_i = 0
+
+    return (avg_u + avg_i) / 2
+
+
+def unit_test(test_index, folds, users, items):
+    test_set = folds[test_index]
+    empty_set = test_set.copy()
+    empty_set['rating'] = 0
+    train_set = pd.concat(folds[:test_index] + [empty_set] + folds[test_index + 1:])
+
+    model = train(train_set, users, items)
+
+    err = []
+    for _, vote in test_set.iterrows():
+        pred = predict(vote["user.id"], vote["item.id"], model)
+        err.append((vote["rating"] - pred) ** 2)
+
+    return np.mean(err)
+
+
+def cross_validation(votes, users, items, n_folds=10, verbose=False):
+    folds = k_fold(n_folds, votes)
+    err = []
+    for i in range(n_folds):
+        err.append(unit_test(i, folds, users, items))
+    if (verbose): print(err)
+    return np.mean(err)
+
+# EndRegion
--- TP1/main.py
+++ TP1/main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-import scipy as sp
 from scipy.sparse import csr_matrix
-#Region[Blue] Init
+
+from cross_valid import cross_validation
+
+#Region[Blue] Init : read csv data
 
 users = pd.read_csv("./data/u.csv", sep="|")
 n_users = len(users)
@@ -15,41 +17,10 @@ n_votes = len(votes)
 votes['user.id'] -= 1
 votes['item.id'] -= 1
 
-m_votes = csr_matrix((votes['rating'], (votes['user.id'], votes['item.id']))).toarray()
-
-
-def k_fold(nb_fold, nb_data):
-    random_state = np.random.RandomState(1)
-    fold_rand_value = random_state.rand(nb_data)
-
-    return np.array([(1/nb_fold*i < fold_rand_value) == (fold_rand_value < 1/nb_fold*(i+1)) for i in range(nb_fold)])
-
-
-fold_array = k_fold(10,n_votes)
-for i in range(len(fold_array)):
-    print(np.sum(fold_array[i]))
-    print(fold_array[i])
-
-votes['rating'][fold_array[5]] = -1 #replace rating value in votes by a value
-print(votes)
-
 #EndRegion
 
-#Region[Green] Average votes prediction
+#Region[Cyan] Question 1
 
-avg_votes = np.zeros(m_votes.shape)
-avg_votes[1,1] = 1
-
-def avg_calc(r, c, m):
-    avg_r = np.mean( m[r][m[r] > 0] )
-    avg_c = np.mean( m[:, c][m[:, c]>0] )
-    return (avg_r+avg_c)/2
-
-for u in range(n_users):
-    for i in range(n_items):
-        avg_votes[u,i] = avg_calc(u,i,m_votes)
-
-print(avg_votes)
-
+result_q1 = cross_validation(votes, users, items, 10, True)
+print(result_q1)
 #EndRegion
-
