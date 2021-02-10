11f50e17145d90eff19b10afd64ab1ad8f700a21
moi <h.canneddu@gmail.com>
2021-02-10 14:39:32 +0100

    k_means

--- TP1/main.py
+++ TP1/main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 
 from scripts.cross_valid import cross_validation
+#from item_item import ii_cos_matrix
+from scripts.agglomeration import agglomeration
 
-#Region[Blue] Init : read csv data
+QUESTION = "Q4"
+
+# Region[Blue] Init : read csv data
 
 users = pd.read_csv("./data/u.csv", sep="|")
 n_users = len(users)
@@ -17,10 +22,41 @@ n_votes = len(votes)
 votes['user.id'] -= 1
 votes['item.id'] -= 1
 
-#EndRegion
+model = csr_matrix((votes['rating'], (votes['user.id'], votes['item.id']))).toarray()
 
-#Region[Cyan] Question 1
+# EndRegion
 
-result_q1 = cross_validation(votes, users, items, 10, False)
-print(result_q1)
-#EndRegion
+if QUESTION == "Q1" or QUESTION == "ALL":
+    # Region[Cyan] Question 1
+
+    result_q1 = cross_validation(votes, users, items, 10, False)
+    print("Q1. Erreur quadratique moyenne : " + str(result_q1))
+
+# EndRegion
+
+"""if QUESTION == "Q2" or QUESTION == "ALL":
+    # Region[Cyan] Question 2
+
+    cos_matrix = ii_cos_matrix(model)
+    triangular_cos = np.tril(cos_matrix)
+
+    # Question 2.a.
+
+    plt.hist(triangular_cos[triangular_cos > 0], color='cyan', edgecolor='black', bins=250)
+    plt.title("Q2.a. Distribution des similarité")
+    plt.ylabel("Nombre d'occurence")
+    plt.xlabel("Valeur de cosinus")
+    plt.show()
+
+    p_zeros = 1 - np.count_nonzero(cos_matrix) / (2 * n_items * (n_items + 1) / 2)
+    print("Q2.a. Proportion de poids nuls : " + str(p_zeros * 100)[:4] + " %")
+
+    # Question 2.b.
+
+# EndRegion"""
+
+if QUESTION == "Q4" or QUESTION == "ALL":
+    cluster_size = [5,10,20,40,80]
+    for size in cluster_size:
+        err = agglomeration(votes, 5, size)
+        print("La mse avec 5 replis et " + str(size) + " cluster est de " + str(err))
---
+++ TP1/scripts/agglomeration.py
@@ -0,0 +1,89 @@
+import warnings
+
+import numpy as np
+import pandas as pd
+import math
+
+from scipy.sparse import csr_matrix
+from sklearn.cluster import KMeans
+
+def k_fold(nb_fold, data):
+    shuffle_data = data.sample(frac=1, random_state=10)
+    folds = []
+    item_per_fold = math.floor(len(shuffle_data) / nb_fold)
+    start, end = 0, item_per_fold
+    for i in range(nb_fold):
+        folds.append(shuffle_data.iloc[start:end, :])
+        start = end
+        end = len(data) if (i + 1 == nb_fold - 1) else end + item_per_fold
+    return folds
+
+def train_test_spit(test_index, folds):
+    test = folds[test_index]
+    empty_set = test.copy()
+    empty_set['rating'] = 0
+    train = pd.concat(folds[:test_index] + [empty_set] + folds[test_index + 1:])
+
+    return train, test
+
+def correlation_matrix(data):
+    val_matrix = np.ones(*data.shape)
+    for i1 in range(data):
+        numeroteur, denominateur = 0
+
+        for i2 in range(i1+1, data):
+
+            value = eval(model[:,i1], model[:,i2])
+            val_matrix[i1,i2] = value
+            val_matrix[i2,i1] = value
+
+
+def compute_clusters_mean(data, clusters, nb_cluster):
+    result = []
+    for i in range(nb_cluster):
+        mask = np.ma.masked_where(clusters == i, clusters).mask
+        masked_data = data[mask]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            mean = np.true_divide(masked_data.sum(0), (masked_data != 0).sum(0))
+        result.append(mean)
+
+    return result
+
+
+def predict(uid, iid, clusters, clusters_mean):
+    cluster_num = clusters[uid]
+    return clusters_mean[cluster_num][iid]
+
+
+def mse(test_set, clusters, clusters_mean):
+    err = []
+    for _, vote in test_set.iterrows():
+        pred = predict(vote["user.id"], vote["item.id"], clusters, clusters_mean)
+        err.append((vote["rating"] - pred) ** 2)
+
+    return np.nanmean(err)
+
+
+def agglomeration(data, n_folds, n_cluster):
+    folds = k_fold(n_folds, data)
+    err = []
+    for i in range(n_folds):
+        train_set_pd, test_set_pd = train_test_spit(i, folds)
+        train_set_np = csr_matrix((train_set_pd['rating'], (train_set_pd['user.id'], train_set_pd['item.id']))).toarray()
+
+        cor = np.corrcoef(train_set_np)
+        kmeans = KMeans(n_clusters=n_cluster, random_state=0).fit(cor)
+        clusters = kmeans.labels_
+        clusters_mean = compute_clusters_mean(train_set_np, clusters, n_cluster)
+        err.append(mse(test_set_pd, clusters, clusters_mean))
+
+    return np.mean(err)
+
+
+
+"""random_N = np.random.rand(*train_set_np.shape)
+random_N = random_N - 0.5
+random_N = random_N/10000
+
+train_set_np = train_set_np + random_N"""
