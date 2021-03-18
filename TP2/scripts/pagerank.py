3ec7d4ec62b3ca8c06a15c316f7d60f185459c99
moi <h.canneddu@gmail.com>
2021-03-18 17:48:47 +0100

    call

--- TP2/scripts/pagerank.py
+++ TP2/scripts/pagerank.py
@@ -1,23 +1,24 @@
 import pandas as pd
 import numpy as np
 
-def compute_pagerank(adjacent, d=0.85):
+
+def compute_pagerank(adjacent, d=0.85, e=1e-6):
     adjacent_temp = adjacent.copy()
     np.fill_diagonal(adjacent_temp.values, 1)
     adjacent_temp[adjacent_temp > 1] = 1
 
     n = adjacent_temp.shape[0]
-    r = pd.Series(1, index=adjacent_temp.index)
+    r = pd.Series(1 / n, index=adjacent_temp.index)
     s = adjacent_temp.sum(axis=1)
     adjacent_t = adjacent_temp.transpose()
 
-    last_rsum = 10000
+    last_r = r.copy()
     convergence = False
     while not convergence:
         r = (1 - d) / n + d * adjacent_t.dot(r / s)
-        r_sum = r.sum()
-        if last_rsum - r_sum < 0.01: convergence = True
-        last_rsum = r_sum
+        norm = np.linalg.norm((last_r - r).to_numpy())
+        if norm < e: convergence = True
+        last_r = r.copy()
 
     return r
 
@@ -26,7 +27,7 @@ def get_top_pagerank(doc, adjacent, pagerank):
     doc_ref = adjacent.loc[doc]
     adjacent[adjacent > 1] = 1
     local_pagerank = doc_ref * pagerank
-    result = local_pagerank[local_pagerank>0]
+    result = local_pagerank[local_pagerank > 0]
     result.sort_values(ascending=False, inplace=True)
-    #top_page = local_pagerank.nlargest(top)
+    # top_page = local_pagerank.nlargest(top)
     return result
