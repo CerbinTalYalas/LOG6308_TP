0a52e71611a0939a640f9a10bdc5814d76397bf0
Grégoire Chapeaux <gchapeaux@gmail.com>
2021-03-19 15:08:49 -0400

    Update cross_validation.py

--- TP2/scripts/cross_validation.py
+++ TP2/scripts/cross_validation.py
@@ -6,18 +6,21 @@ from scipy.sparse import csr_matrix
 adjacent = pd.read_table("./data/citeseer.rtable", sep=" ")
 adjacent.columns = adjacent.columns.astype('int64')
 
-def gen_sets(adjacent_values, p=0.1):
+def gen_sets(adjacent, p=0.1):
+    adjacent_train = adjacent.copy()
+    adjacent_values = adjacent.values
     l, c = np.where(adjacent_values == 1)
     train_matrix = np.ndarray.copy(adjacent_values)
     i = len(l)
     test_indexes = random.sample(range(i), int(p*i))
     test_set = []
     for index in test_indexes:
-        test = (l[index], c[index])
+        test = (adjacent_train.index[l[index]], adjacent_train.columns[c[index]])
         test_set.append(test)
-        train_matrix[test] = 0
-    return train_matrix, test_set
+        adjacent_train.at[l[index], c[index]] = 0
+    return adjacent_train, test_set
 
-tr, te = gen_sets(adjacent.values)
+
+tr, te = gen_sets(adjacent)
 print(tr)
-print(len(te))
+print(te)
